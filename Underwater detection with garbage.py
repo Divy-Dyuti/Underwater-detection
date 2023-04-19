6d38ce6aa5e2a9ffacee311147ede2817a086d05
Divy-Dyuti <69088716+Divy-Dyuti@users.noreply.github.com>
2023-04-19 21:10:40 +0530

    Update Underwater detection with garbage.py

--- Underwater detection with garbage.py
+++ Underwater detection with garbage.py
@@ -3,16 +3,13 @@ import numpy as np
 from tensorflow.keras.models import load_model
 
 # Load the trained model
-model = load_model("/keras_model.h5")
+model = load_model("D:/model/keras_model.h5")
 
 # Define the classes
-classes = ['dolphin', 'fish', 'seahorse', 'shark', 'garbage']
+classes = ['dolphin', 'fish', 'seahorse', 'shark', 'human', 'garbage']
 
 # Set up the webcam
 cap = cv2.VideoCapture(0)
-
-# Initialize the bounding box
-bbox = None
 
 while True:
     # Capture a frame from the webcam
@@ -30,32 +27,29 @@ while True:
     # Make a prediction with the model
     predictions = model.predict(input_frame)
 
-    # Get the class with the highest probability
-    class_index = np.argmax(predictions[0])
-    class_name = classes[class_index]
+    # Get the indices of the classes with high probability
+    class_indices = np.where(predictions[0] > 0.5)[0]
 
-    # Draw a bounding box around the object
-    if class_name == 'garbage':
-        color = (0, 0, 255)  # red
-    else:
-        color = (0, 255, 0)  # green
+    # Loop over the detected classes and draw a bounding box around each object
+    for class_index in class_indices:
+        class_name = classes[class_index]
 
-    if bbox is not None:
-        x, y, w, h = bbox
-        cv2.rectangle(frame, (x, y), (x+w, y+h), color, thickness=2)
+        # Draw a bounding box around the object
+        if class_name == 'garbage':
+            color = (0, 0, 255)  # red
+        else:
+            color = (0, 255, 0)  # green
 
-    # Update the bounding box with the coordinates of the detected object
-    mask = predictions[0] > 0.5
-    contours, _ = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours) > 0:
-        x, y, w, h = cv2.boundingRect(contours[0])
-        bbox = (x, y, w, h)
-    else:
-        bbox = None
+        # Update the bounding box with the coordinates of the detected object
+        mask = predictions[0] == predictions[0][class_index]
+        contours, _ = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            x, y, w, h = cv2.boundingRect(contours[0])
+            cv2.rectangle(frame, (x, y), (x+w, y+h), color, thickness=2)
 
-    # Display the class name
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(frame, class_name, (10, 30), font, 1, (0, 0, 255), 2)
+            # Display the class name
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame, class_name, (x, y-10), font, 1, color, 2)
 
     # Display the frame
     cv2.imshow('Object Detection', frame)
